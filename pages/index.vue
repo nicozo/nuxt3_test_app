0a16f6b1814fc9d2a7c39473f3deeb970ac78b0f
nicozo <niczo.soccer161@gmail.com>
2023-01-13 19:15:36 +0900

    Add: プラグインファイルを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
     <h1>Main Page</h1>
-    <div>Count:{{ count }}</div>
-    <div>
-      <button @click="() => inc()">increase</button>
-      <button @click="() => dec()">decrease</button>
-    </div>
+    <h2>{{ $hello('World') }}</h2>
   </div>
 </template>
 
 <script setup>
-const { count, inc, dec } = useCounter(100);
+const { $hello } = useNuxtApp();
 </script>
