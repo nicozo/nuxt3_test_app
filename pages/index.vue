d7401ff6eb987c6a61cb484a17a27a34ce4a38a1
nicozo <niczo.soccer161@gmail.com>
2023-01-13 13:10:27 +0900

    Add: composablesファイルを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Main Page</h1>
+  <div>
+    <h1>Main Page</h1>
+    <div>Count:{{ count }}</div>
+    <div>
+      <button @click="() => inc()">increase</button>
+      <button @click="() => dec()">decrease</button>
+    </div>
+  </div>
 </template>
+
+<script setup>
+const { count, inc, dec } = useCounter(100);
+</script>
